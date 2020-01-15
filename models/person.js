42d93780710e99e93dfb6fb7156d5fdfeabcc751
Arjun <arjun.anandk@gmail.com>
2020-01-15 19:43:42 +0000

    Task 1 Complete

--- models/person.js
+++ models/person.js
@@ -1,1 +1,38 @@
 // Your Person class goes here!
+
+class Person {
+    constructor(firstName, surname, dob) {
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
+        this.surname = surname.charAt(0).toUpperCase() + surname.slice(1)
+        this.dob = dob
+        this.emails = []
+        this.numbers = []
+    }
+
+    fullname() {
+        return this.firstName + ' ' + this.surname
+    }
+
+    addEmail(email){
+        this.emails.push(email)
+
+    }
+
+    addPhoneNumber(num){
+        this.numbers.push(num)
+    }
+
+    returnFormattedDetails() {
+        return this.fullname() + '\n' + "---------- \n"+
+        "DOB: "+this.dob + '\n' + "Email Addresses: " + '\n' +this.emails +
+        '\n' + "Phone Number(s): \n" + this.numbers
+
+    }
+
+}
+
+let person = new Person('arjun', 'anand', '2 Apr 2001', "arjun.anand@ada.ac.uk", "07123456789");
+console.log(person.returnFormattedDetails());
+
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,56 @@
 const chai = require('chai')
 const expect = chai.expect
 
+const Person = require('../models/person.js')
 // Your Person test goes here!
+describe ('Person', () => {
+    it('should initialize properly', () => {
+        var person = new Person('Arjun', 'Anand', '1 Jan 1990')
+        expect(person.firstName).to.equal('Arjun')
+        expect(person.surname).to.equal('Anand')
+        expect(person.dob).to.equal('1 Jan 1990')
+        expect(person.emails).to.deep.equal([])
+        expect(person.numbers).to.deep.equal([])
+    })
+
+    it('should capitalise the first name', () => {
+        var person = new Person('amy', 'Nother', '1 Jan 1990')
+    
+        expect(person.firstName).to.equal('Amy')
+        expect(person.surname).to.equal('Nother')
+        expect(person.dob).to.equal('1 Jan 1990')
+    })
+    
+    it('shoulds reutrn a full name', ()=>{
+        var person = new Person('Arjun', 'Anand', '1 Jan 1990')
+    
+        expect(person.fullname()).to.equal('Arjun Anand')
+    })
+
+    it('should append new emails to the empty array', ()=>{
+        var person = new Person('Arjun', 'Anand', '1 Jan 1990')
+        person.addEmail('arjun.anand@ada.ac.uk')
+        person.addEmail('arjun@anand.com')
+
+        expect(person.emails).to.deep.equal([
+            'arjun.anand@ada.ac.uk',
+            'arjun@anand.com'
+        ])
+        console.log(person.returnFormattedDetails());
+    })
+
+    it('should append new phone numbers to the empty array', ()=>{
+        var person = new Person('Arjun', 'Anand', '1 Jan 1990')
+        person.addPhoneNumber('07123456789')
+        person.addPhoneNumber('98765432180')
+        expect(person.numbers).to.deep.equal([
+            '07123456789',
+            '98765432180'
+        ])
+        console.log(person.returnFormattedDetails());
+    }) 
+    
+    
+    
+})
+
